c51ac7474ac48a533735721c24a00eaaeb63f2ac
tuomasahva <tuomas.ahva@gmail.com>
2015-11-18 17:41:33 +0200

    Update and rename variable to variable.py

---
+++ public/examples/variable.py
@@ -0,0 +1,7 @@
+# Exercise: Let's check your name
+
+# Write your name like this
+# name = "Tuomas"
+
+# We can see your name in the console if you write down like this
+# print("Your name is: " + name)

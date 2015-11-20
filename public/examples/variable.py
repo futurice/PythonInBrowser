c0dc6354aa3f1602ff5fa565a9f4b88340060c24
Tuomas Ahva <tuomas.ahva@futurice.com>
2015-11-20 11:34:52 +0200

    Edited variable exercise

--- public/examples/variable.py
+++ public/examples/variable.py
@@ -1,7 +1,26 @@
-# Exercise: Let's check your name
+# Exercise: Let's check if the computer can remember your name
+# This can be done using a variable
 
-# Write your name like this
-# name = "Tuomas"
+# Write your name like this, but replace Tuomas with your own name.
+name = "Tuomas"
 
-# We can see your name in the console if you write down like this
-# print("Your name is: " + name)
+# 'name' is a variable.
+# '=' is used to assign a value to a variable.
+# As the name says, the value of a variable can vary.
+# "'Tuomas'" is a value that is assigned to the variable.
+# When assigning text to a variable, it needs to be written in quotation marks.
+
+# Your name will be printed in the console if you write down like this
+print "Your name is: " + name
+
+# ...and click 'Run' on the upper right corner.
+# See, computer now remembers your name. It's been saved in a variable called 'name'.
+
+# Now, let's change the contents of 'name' to something else.
+# Write another name on line 5, after 'name' and '=', for example like this
+# name = "Tiina". Don't change anything else.
+
+# Click 'Run' again on the upper right corner and check what is printed on the console.
+
+# Congratulations! You've just saved data in a variable and also changed its value.
+# Variable is a very essential concept in programming.

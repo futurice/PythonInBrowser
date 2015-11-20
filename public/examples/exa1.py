2e47fef91d155de1e67d670af0b1ac7313b71606
Tuomas Ahva <tuomas.ahva@futurice.com>
2015-11-20 13:35:01 +0200

    Added more comment text in exa1.py

--- public/examples/exa1.py
+++ public/examples/exa1.py
@@ -3,3 +3,18 @@
 # Press 'Run'-button and see what happens
 
 print "Hello world"
+
+# All the text in the editor that comes after '#' are comments.
+# When the code in the editor runs (when you click the 'Run' button)
+# all the lines which are marked as comments with '#' are ignored.
+# Other lines are processed.
+
+# There are 3 significant areas in this coding environment: editor, canvas and console.
+# Editor is where you write all the code.
+# Canvas is used for showing the graphical output.
+# Console is used for showing text output.
+
+# Every time you press 'Run' the code is processed and the outputs are shown in either canvas or console, or both.
+
+# Just to make you feel more comfortable with the environment, press 'Run' once again.
+# Now you can move on the next exercise.

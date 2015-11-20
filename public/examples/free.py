6637fbea3ebf786d8aaba819b1b98b3c7ec3e330
Antti Ajanki <antti.ajanki@iki.fi>
2015-11-20 16:25:05 +0200

    Draw a letter task

--- public/examples/free.py
+++ public/examples/free.py
@@ -1,11 +1,21 @@
 # free.py
 #
 # Now you can freely use things you have learned so far.
-# Try printing different kind of things and move turtle on the screen.
+# For example, use the turtle to draw the first letter of
+# your name or other objects on the screen. 
+# 
 # Remember these turtle commands:
 # t.left(90)
 # t.right(90)
 # t.forward(50)
+#
+# Other commands you can try:
+# t.penup()
+# t.pendown()
+# t.goto(100, 50)
+#
+# Open the Turtle methods at the bottom of this page for even
+# more commands.
 
 import turtle
 t = turtle.Turtle()

4a585afd63ce4afa6ae7474e1fc4e907d55afd01
Tiina Romu <tiina.romu@futurice.com>
2015-12-16 10:20:02 +0200

    session 4 finalized

--- public/examples/session4/arena.py
+++ public/examples/session4/arena.py
@@ -5,9 +5,17 @@ import codeclub
 import turtle
 
 t = turtle.Turtle()
-codeclub.drawArena(t)
+codeclub.drawFirstArena(t)
 
 ##### EXERCISE #####
+# 1.
 # Can you solve the track with Turtle?
 # Use only commands forward, right, left, speed
-#
+
+codeclub.drawFirstArena(t)
+
+# 2.
+# Can you solve the track with Turtle?
+# Use only commands forward, right, left, speed
+
+#codeclub.drawTriangleArena(t)

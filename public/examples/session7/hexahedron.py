75a90d7b5fb91cd9728a51cd8ab78af5df74ace1
Tiina Romu <tiina.romu@futurice.com>
2016-01-25 17:41:42 +0200

    session 7 exercises added

---
+++ public/examples/session7/hexahedron.py
@@ -0,0 +1,36 @@
+import turtle
+t = turtle.Turtle()
+
+
+##### EXERCISE #####
+# Fill the functions so that you can draw 3D hexahedron.
+# Note that you should use the measures
+# given in the definition of hexahedron.
+#
+
+# Function to draw frontside, fill this
+def frontside(w, h):
+  print "This function should draw the front side of function"
+
+# Function to draw topside, fill this
+def topside(w, d):
+  print "This function should draw the top side of function"
+
+# Function to draw rightside, fill this
+def rightside(h, d):
+  print "This function should draw the right side of function"
+
+# Fill the function frontside, topside and rightside to make this function draw hexahedron
+# Here w is width, h is height and d is depth
+
+def hexahedron(w, h, d):
+  frontside(w, h)
+  topside(w, d)
+  rightside(h, d)
+
+t.penup()
+t.goto(0, -200)
+t.pendown()
+hexahedron(50, 50, 50)
+hexahedron(200, 100, 200)
+hexahedron(70, 100, 200)
---
+++ public/examples/session7/magic.py
@@ -0,0 +1,71 @@
+# magic.py
+# What shape?
+
+##### INFO #####
+import turtle
+
+##### EXERCISE #####
+# Your job is to find out what shape you get drawing this way.
+# First read the instruction and try to imagine the shape.
+# Only after this draw it with a turtle.
+
+# Step 1
+# Draw to X -letter so that they form a diamond shape between them like this: XX
+# In the X the lenght of lines is equal and the angle is 45 degrees
+
+# Step 2
+# From the upper and bottom vertexes of the diamond draw straigt line up and down
+# The lenght of the line is half of the side of the X.
+# Your drawing should look something like this:
+#
+
+# Step 3
+# After this join all the points with a line so that you have continuous perimeter
+#
+
+# Scroll down to get hints.
+
+# Write your solution here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## If you need help, here is some tips on how to draw each step:
+
+# Step 1
+#
+# XX
+#
+
+# Step 2
+#
+#   |
+#  X X
+#   |
+#
+
+# Step 3
+#
+#  /|\
+# |X X|
+#  \|/
+#

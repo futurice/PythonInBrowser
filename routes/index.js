5c7d56711c468e5cec7b92fbce5809537bd6b215
Antti Ajanki <antti.ajanki@futurice.com>
2017-02-18 12:38:13 +0200

    Select the exercise language on the front page

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,26 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/codeclub', function(req, res, next) {
-  var exercises = require('../examples/examples.json');
+  var exercisesByLanguage = {
+    'en': '../examples/examples.json',
+    'fi': '../examples/examples-fi.json'
+  };
+  var exerciseFile = exercisesByLanguage[req.query.lang || 'en'];
+  var exercises = require(exerciseFile);
   var modules = require('../examples/modules.json');
-  console.log(JSON.stringify(modules, 2));
-  res.render('codeclub', { title: 'Chilicorn Code Club', exercises: exercises, modules: modules });
+  res.render('codeclub', {
+    title: 'Chilicorn Code Club',
+    exercises: exercises,
+    modules: modules
+  });
 });
 
 router.get('/material', function(req, res, next) {
   var materialData = require('../examples/material.json');
-  res.render('material', { title: 'Chilicorn Code Club', material: materialData.material});
+  res.render('material', {
+    title: 'Chilicorn Code Club',
+    material: materialData.material
+  });
 });
 
 module.exports = router;

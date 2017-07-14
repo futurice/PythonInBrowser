adfa59f120f75636a68e46bac2dfc7777bf40acc
Antti Ajanki <antti.ajanki@futurice.com>
2017-07-14 19:38:24 +0300

    Remove unused cookie-parser and kerberos

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var exphbs = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var Promise = require('bluebird');
@@ -25,7 +24,6 @@ function makeApp() {
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
   app.use(require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
